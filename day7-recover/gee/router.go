8ab7a916e85f3f4b6acc7c9d058ce49e4d194ad9
Sanagiig <laiwenjunhhh@163.com>
2022-09-09 11:36:06 +0800

    day7

---
+++ day7-recover/gee/router.go
@@ -0,0 +1,87 @@
+package gee
+
+import (
+	"net/http"
+	"strings"
+)
+
+type Router struct {
+	roots    map[string]*node
+	handlers map[string]HandlerFunc
+}
+
+func NewRouter() *Router {
+	return &Router{
+		make(map[string]*node),
+		make(map[string]HandlerFunc),
+	}
+}
+
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
+}
+
+func (r *Router) addRoute(method string, pattern string, handler HandlerFunc) {
+	parts := parsePattern(pattern)
+
+	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
+	r.handlers[key] = handler
+}
+
+func (r *Router) getRoute(method string, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+
+	if !ok {
+		return nil, nil
+	}
+
+	n := root.search(searchParts, 0)
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index]
+			}
+
+			// *part/x/y/z 将 *part 后的 /x/y/z 作为val 簇内 params.part
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+
+	return nil, nil
+}
+
+func (r *Router) handle(c *Context) {
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		c.handlers = append(c.handlers, r.handlers[key])
+	} else {
+		c.String(http.StatusNotFound, "404 Not Found: %v", c.Path)
+	}
+
+	c.Next()
+}
