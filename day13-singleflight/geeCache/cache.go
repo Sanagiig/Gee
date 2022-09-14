85ec160a255e1a6f4a964aef1b9b2e5a280be6ef
Sanagiig <laiwenjunhhh@163.com>
2022-09-14 21:37:42 +0800

    day13

---
+++ day13-singleflight/geeCache/cache.go
@@ -0,0 +1,47 @@
+package geeCache
+
+import "sync"
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *LRUCache
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.lru == nil {
+		c.lru = NewLRUCache(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.lru == nil {
+		return
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+
+	return
+}
+
+// A Getter loads data for a key.
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// A GetterFunc implements Getter with a function.
+type GetterFunc func(key string) ([]byte, error)
+
+// Get implements Getter interface function
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
