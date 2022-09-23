78e0021c96a5262a56cf918ba9abdb594652093a
Sanagiig <laiwenjunhhh@163.com>
2022-09-23 10:04:18 +0800

    gi: add gi, giCache

--- finally-gi/giCache/singleFlight/singleFlight.go
+++ finally-gi/giCache/singleFlight/singleFlight.go
@@ -1,7 +1,6 @@
 package singleFlight
 
 import (
-	"log"
 	"reflect"
 	"runtime"
 	"strings"
@@ -35,13 +34,13 @@ func (c *caller) Exc() {
 
 // 单飞处理 ， 防止缓存击穿
 type SingleFlight struct {
-	rwmu        sync.RWMutex
+	mu          sync.Mutex
 	callerGroup map[string]*caller
 }
 
 func NewSingleFlight() *SingleFlight {
 	return &SingleFlight{
-		rwmu:        sync.RWMutex{},
+		mu:          sync.Mutex{},
 		callerGroup: make(map[string]*caller),
 	}
 }
@@ -53,23 +52,23 @@ func (s *SingleFlight) addCaller(fn CallerFunc, callerKey string, args ...string
 }
 
 func (s *SingleFlight) getCaller(callerKey string) *caller {
-	s.rwmu.RLock()
-	defer s.rwmu.RUnlock()
 	return s.callerGroup[callerKey]
 }
 
 func (s *SingleFlight) Do(fn CallerFunc, args ...string) (any, error) {
+	s.mu.Lock()
+
 	fnName := runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
 	callerKey := fnName + strings.Join(args, "-")
 	curCaller := s.getCaller(callerKey)
-	log.Printf("caller key : %s", callerKey)
+
 	if curCaller == nil {
 		// 此处如果 Lock 放在 addCaller外 是为了防止 Exc 执行错误，删除组
 		// 时在 别的 goroutine 获取到异常数据
 		// 但是放在 addCaller 内部影响也不大，就某些 goroutine 获取到错误，会重新请求
 
-		s.rwmu.Lock()
 		curCaller = s.addCaller(fn, callerKey, args...)
+		s.mu.Unlock()
 		curCaller.Exc()
 
 		// 清空
@@ -77,10 +76,10 @@ func (s *SingleFlight) Do(fn CallerFunc, args ...string) (any, error) {
 			delete(s.callerGroup, callerKey)
 		}
 
-		s.rwmu.Unlock()
 		return curCaller.value, curCaller.err
 	}
 
+	s.mu.Unlock()
 	curCaller.wg.Wait()
 	return curCaller.value, curCaller.err
 }
--- finally-gi/giCache/singleFlight_test.go
+++ finally-gi/giCache/singleFlight_test.go
@@ -1,37 +1,12 @@
-package singleFlight
+package giCache
 
 import (
-	"finally-gi/giCache"
 	"fmt"
-	"io"
 	"net/http"
-	"strings"
 	"sync"
 	"testing"
 	"time"
 )
-
-type httpRequest = func(url string) (resp *http.Response, err error)
-
-func request(t testing.TB, reqFunc httpRequest, url string, result string) {
-	t.Helper()
-	res, err := reqFunc(url)
-	if err != nil {
-		t.Fatalf("[%30s] err \n %v\n", url, err.Error())
-	}
-
-	defer res.Body.Close()
-
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	if !strings.EqualFold(string(data), result) {
-
-		t.Fatalf("[%s] err\n expected:  \"%v\" \n get wrong data: \"%v\" \n", url, result, string(data))
-	}
-}
 
 func TestSingleFlight(t *testing.T) {
 	var data = map[string]string{
@@ -48,9 +23,11 @@ func TestSingleFlight(t *testing.T) {
 	addr := "localhost:8888"
 	baseUrl := "http://" + addr
 	count := 0
-	cs := giCache.NewCacheServer(addr)
-	c1 := giCache.New("test", 2<<10,
-		giCache.GetterFunc(func(key string) ([]byte, error) {
+	testTimes := 1000
+
+	cs := NewCacheServer(addr)
+	c1 := New("test", 2<<10,
+		GetterFunc(func(key string) ([]byte, error) {
 			v := data[key]
 			if v == "" {
 				return nil, fmt.Errorf("data[%s] not found\n", key)
@@ -62,13 +39,14 @@ func TestSingleFlight(t *testing.T) {
 		nil,
 	)
 
+	oo := 0
 	wg := sync.WaitGroup{}
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < testTimes; i++ {
 		for path, result := range getReq {
 			url := baseUrl + path
 			wg.Add(1)
+			oo++
 			go func(url string, result string) {
-				time.Sleep(time.Second)
 				request(t, http.Get, url, result)
 				wg.Done()
 			}(url, result)
@@ -79,6 +57,7 @@ func TestSingleFlight(t *testing.T) {
 	go cs.Run()
 
 	wg.Wait()
+	time.Sleep(time.Second)
 	if l := len(data); count > l {
 		t.Fatalf("get remote data times is too much . expected [%d] but got [%d]", l, count)
 	}
